a85c156f33bcee9eaafa9e0c6ce4e05302a34231
Anton Kirillov <akirillov.mailbox@gmail.com>
2013-11-27 22:36:01 +0400

    (+) Initial routes created

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,26 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
-  
+
+  def getToken() = Action {
+    Ok("got getToken request")
+  }
+
+  def postLink(code: String) = Action {
+    Ok("got postLink request "+code)
+  }
+
+  def getLink(code: String) = Action {
+    Ok("got getLink request "+code)
+  }
+
+  def getFolder(id: String) = Action {
+    Ok("got getFolder request "+id)
+  }
+
+  def getClicks(code: String) = Action {
+    Ok("got getClicks request "+code)
+  }
 }
